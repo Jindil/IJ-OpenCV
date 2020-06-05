3da6fba31dfb9ac721fecedf494f391d6c69772b
Laurent Thomas <laurent132.thomas@laposte.net>
2020-06-05 16:34:40 +0200

    Create openCL.py

---
+++ src/main/java/ijopencv/examples/openCL.py
@@ -0,0 +1,23 @@
+#@ImagePlus imp
+from org.bytedeco.javacpp.opencv_core import Mat, GpuMat, UMat, Size
+from org.bytedeco.javacpp.opencv_imgproc import blur
+from ijopencv.ij     import ImagePlusMatConverter as ImpToMat
+from ijopencv.opencv import MatImagePlusConverter as MatToImp
+
+# I - Convert the ImagePlus to an opencv matrix
+
+imCV = ImpToMat.toMat(imp.getProcessor())
+print imCV
+#print imCL
+
+# CPU -> OK
+blured = Mat()
+kernel = Size(5,5)
+blur(imCV, blured, kernel)
+print blured
+
+# GPU
+imCL = UMat(imCV)
+bluredCL = UMat(imp.height, imp.width, 0) # 8bit
+blur(imCL, bluredCL, kernel)
+print bluredCL
