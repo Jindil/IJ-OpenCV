9cae19c20794d79b1c163b2fab78b580ea830161
Laurent Thomas <laurent132.thomas@laposte.net>
2020-06-05 17:13:14 +0200

    success

--- src/main/java/ijopencv/examples/openCL.py
+++ src/main/java/ijopencv/examples/openCL.py
@@ -1,16 +1,16 @@
 #@ImagePlus imp
-from org.bytedeco.javacpp.opencv_core import Mat, GpuMat, UMat, Size
+from org.bytedeco.javacpp.opencv_core import Mat, GpuMat, UMat, Size, ACCESS_READ
 from org.bytedeco.javacpp.opencv_imgproc import blur
 from ijopencv.ij     import ImagePlusMatConverter as ImpToMat
 from ijopencv.opencv import MatImagePlusConverter as MatToImp
 from org.bytedeco.javacpp.opencv_core import Device
+from ij import ImagePlus
 
 dev = Device.getDefault()
 
 # I - Convert the ImagePlus to an opencv matrix
-
 imCV = ImpToMat.toMat(imp.getProcessor())
-print imCV
+#print imCV
 #print imCL
 
 # CPU -> OK
@@ -20,7 +20,17 @@ blur(imCV, blured, kernel)
 print blured
 
 # GPU
-imCL = UMat(imCV)
-bluredCL = UMat(imp.height, imp.width, 0) # 8bit
+#imCL = UMat(imCV) # this crashes when called then
+imCL = imCV.getUMat(ACCESS_READ) 
+bluredCL = imCL.clone()
+
 blur(imCL, bluredCL, kernel)
 print bluredCL
+
+# CL to CV
+bluredCV = bluredCL.getMat(ACCESS_READ)
+
+# CV to ImageJ
+imProc = MatToImp.toImageProcessor(bluredCV)
+impNew = ImagePlus("test", imProc)
+impNew.show()
