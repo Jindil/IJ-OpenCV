9c8ffc1ea1589c55a1a4396048b613525a9500eb
Laurent Thomas <laurent132.thomas@laposte.net>
2020-06-05 16:45:17 +0200

    add device

--- src/main/java/ijopencv/examples/openCL.py
+++ src/main/java/ijopencv/examples/openCL.py
@@ -3,6 +3,9 @@ from org.bytedeco.javacpp.opencv_core import Mat, GpuMat, UMat, Size
 from org.bytedeco.javacpp.opencv_imgproc import blur
 from ijopencv.ij     import ImagePlusMatConverter as ImpToMat
 from ijopencv.opencv import MatImagePlusConverter as MatToImp
+from org.bytedeco.javacpp.opencv_core import Device
+
+dev = Device.getDefault()
 
 # I - Convert the ImagePlus to an opencv matrix
 
